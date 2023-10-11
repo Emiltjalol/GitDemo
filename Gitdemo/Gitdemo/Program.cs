263e358347c4fd527209b7e14b9a25d3ae07a680
Emiltjalol <Emil.ohrstrom@hotmail.com>
2023-10-11 11:41:35 +0200

    First commit

---
+++ Gitdemo/Gitdemo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Gitdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hejsan världen!");
+            
+        }
+    }
+}
